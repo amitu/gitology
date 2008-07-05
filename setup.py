db24e50e85db3ead1c005ae0cf22b1ae1f84df1b
Amit Upadhyay <upadhyay@gmail.com>
2008-07-06 04:15:19 +0530

    added setup.py and restructured the project to make it easier for setuptools

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name = "gitology",
+    version = "0.1",
+    packages = find_packages('src'),
+    package_dir = { '': 'src' },
+
+    scripts = [
+        'src/tools/gitology',
+        'src/tools/gitology-blog',
+    ],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        'hello': ['*.msg'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Amit Upadhyay",
+    author_email = "code@amitu.com",
+    description = "Git based blog/wiki system using DJango",
+    license = "BSD",
+    keywords = "git django blog wiki",
+    url = "http://code.google.com/p/gitology/" 
+)
+
