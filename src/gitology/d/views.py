e8c4228eb9ad782f561b345cfb43a74b9a538dfd
Amit Upadhyay <amitu@amitu-laptop.(none)>
2008-12-23 09:31:39 +0530

    these are the views to be implemented. sort of.

--- src/gitology/d/views.py
+++ src/gitology/d/views.py
@@ -3,6 +3,10 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def index(request):
-    return HttpResponse("OK")
+def show_blog(request, blog_name): pass
+def show_post(request, blog_name, post_slug): pass
+def show_archive(request, blog_name, archive_format): pass
+def show_category(request, blog_name, category_slug): pass
+def show_wiki(request, page): pass
+def add_comment(request, document_name): pass
+def index(request): return HttpResponse("OK")
