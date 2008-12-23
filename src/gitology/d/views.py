baa202a8d1dc7f19b5661f4a408e21b935754219
Amit Upadhyay <amitu@amitu-laptop.(none)>
2008-12-23 11:33:38 +0530

    blog index thing there.

--- src/gitology/d/views.py
+++ src/gitology/d/views.py
@@ -3,7 +3,12 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
 
-def show_blog(request, blog_name): pass
+def show_blog(request, name): 
+    return render_to_response(
+        "blog/index.html",
+        { 'name': name },
+        context_instance = RequestContext(request)
+    )
 def show_post(request, blog_name, post_slug): pass
 def show_archive(request, blog_name, archive_format): pass
 def show_category(request, blog_name, category_slug): pass
