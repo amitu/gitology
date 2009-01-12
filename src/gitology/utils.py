4bbfe71600a012b33945a1b835bece001ce07aad
Amit Upadhyay <amitu@amitu-laptop.(none)>
2009-01-13 02:53:06 +0530

    show repo size

--- src/gitology/utils.py
+++ src/gitology/utils.py
@@ -281,3 +281,26 @@ def smart_wrap(s, width=None):
         else: lines += textwrap.wrap(line, width=width)
     return "\n".join(lines)
 # }}}
+
+# getDirSize {{{ 
+# stolen from: http://mail.python.org/pipermail/python-list/2000-June/037460.html
+def calcDirSize(arg, dir, files):
+    for file in files:
+        stats = os.stat(os.path.join(dir, file))
+        size = stats[6]
+        arg.append(size)
+
+def getDirSize(dir):
+    sizes = []
+    os.path.walk(dir, calcDirSize, sizes)
+    total = 0
+    for size in sizes:
+        total = total + size
+    if total > 1073741824:
+        return (round(total/1073741824.0, 2), 'GB')
+    if total > 1048576:
+        return (round(total/1048576.0, 2), 'MB')
+    if total > 1024:
+        return (round(total/1024.0, 2), 'KB')
+    return (total, 'bytes')
+# }}}
