1f84d5e4618b5c3f91af0309524ae719efe37dda
Amit Upadhyay <amitu@amitu-laptop.(none)>
2009-02-17 16:23:29 +0530

    /login/ is better url than /openid/

--- src/gitology/d/urls.py
+++ src/gitology/d/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^openid/$', 'django_openidconsumer.views.begin'),
-    (r'^openid/with-sreg/$', 'django_openidconsumer.views.begin', {
+    (r'^login/$', 'django_openidconsumer.views.begin'),
+    (r'^login/with-sreg/$', 'django_openidconsumer.views.begin', {
         'sreg': 'email',
-        'redirect_to': '/openid/complete/',
+        'redirect_to': '/login/complete/',
     }),
-    (r'^openid/complete/$', 'django_openidconsumer.views.complete'),
-    (r'^openid/signout/$', 'django_openidconsumer.views.signout'),
+    (r'^login/complete/$', 'django_openidconsumer.views.complete'),
+    (r'^logout/$', 'django_openidconsumer.views.signout'),
     (r'^document/(?P<name>.*)/$', 'gitology.d.views.show_document'),
 )
