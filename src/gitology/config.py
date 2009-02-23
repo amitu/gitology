537437ac88fad350f1a3ecbd8704652120824458
Amit Upadhyay <amitu@amitu-laptop.(none)>
2009-02-24 00:35:26 +0530

    now settings can be overwritten by a config.rc file in repo

--- src/gitology/config.py
+++ src/gitology/config.py
@@ -13,10 +13,11 @@ If file_name is passed, it will be used.
 import ConfigParser, os, path
 from gitology.utils import attrdict, ImproperlyConfigured
 
-def get_config(file_name=None):
+def get_config(file_name=None, current_config=None):
     config = ConfigParser.ConfigParser()
     config.optionxform = str
-    _config = attrdict()
+    if current_config is None: current_config = attrdict()
+    _config = current_config
     if file_name:
         _config.RC_FILE = path.path(os.path.expanduser(file_name))
     elif "GITOLOGY_CONFIG_FILE" in os.environ:
@@ -29,17 +30,24 @@ def get_config(file_name=None):
         raise ImproperlyConfigured("RC file does not exists: %s" % _config.RC_FILE.abspath())
     config.read(_config.RC_FILE) 
     for section in config.sections():
-        _config[section] = attrdict()
+        if section not in _config:
+            _config[section] = attrdict()
         for k, v in config.items(section):
             _config[section][k] = v
     return _config
 
 def initialize():
     conf = get_config()
+
     if "REPO" not in conf: 
         raise ImproperlyConfigured("No section REPO in config, file=%s." % conf.RC_FILE)
     if "LOCAL" not in conf.REPO: 
         raise ImproperlyConfigured("No setting LOCAL in REPO, file=%s." % conf.RC_FILE)
+    conf.LOCAL_REPO_PATH = path.path(os.path.expanduser(conf.REPO.LOCAL))
+
+    if conf.LOCAL_REPO_PATH.joinpath("config.rc").exists():
+        conf = get_config(conf.LOCAL_REPO_PATH.joinpath("config.rc"), conf)
+
     if "REMOTE" not in conf.REPO: conf.REPO.REMOTE = ""
     if "AUTO_COMMIT" in conf.REPO: 
         conf.REPO.AUTO_COMMIT = { 'True': True }.get(conf.REPO.AUTO_COMMIT, False)
@@ -56,7 +64,6 @@ def initialize():
         conf.DEFAULTS.USE_MD5 = False
     else:
         conf.DEFAULTS.USE_MD5 = { 'True': True }.get(conf.DEFAULTS.USE_MD5, False)
-    conf.LOCAL_REPO_PATH = path.path(os.path.expanduser(conf.REPO.LOCAL))
     import gitter
     gitter.git = gitter.Git(conf.REPO.LOCAL, conf.REPO.REMOTE, conf.REPO.AUTO_COMMIT)
     return conf
